f49845e78c6d0d307b3de4d60257617f5ac12dba
Zeronos <boss16805@gmail.com>
2022-08-04 14:11:44 +0700

    edit quiz2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  p-name.style.display = ""
+  p-email.style.display = ""
+  p-adress.style.display = ""
+  btn-random.style.display = ""
+  if(resp.data[0])
+}
+
+btn-random.onclicked = async () =>{
+  callApi();
 }
